499176714b75ea5d2efbe0674f6fd23752efe4af
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-16 16:23:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ 
+const mapLetters = (str) => {
+  // Write your code here
+ 
+  const obj = {};
+ 
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    obj[char] = obj[char] || [];
+    obj[char].push(i);
+  }
+ 
+  return obj;
+};
+ 
+// Do not change the below code
+ 
+const input = prompt("Enter the string");
+console.log(mapLetters(input.trim()));
